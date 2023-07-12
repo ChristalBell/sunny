f0dd8436fd09bdda382656a8fd34bbeb55017611
Christal Bell <christalvictoriabell@gmail.com>
2023-07-11 22:21:01 -0700

    starting files, Headers.scss error

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import { Container } from "@mui/material";
+import "./Headers.scss";
+
+const Header = () => {
+  return (
+    <Container>
+      <ul>
+        <li>sunnyside</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </Container>
+  );
+};
+
+export default Header;
