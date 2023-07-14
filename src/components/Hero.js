a64c529af9511087c9d28e115ce4b029a7cee20c
Christal Bell <christalvictoriabell@gmail.com>
2023-07-13 21:03:35 -0700

    Hero complete

---
+++ src/components/Hero.js
@@ -0,0 +1,20 @@
+const Hero = () => {
+  return (
+    <div>
+      <h1
+        style={{
+          color: "white",
+          margin: "3rem",
+          marginBottom: "6rem",
+          fontSize: "4.5rem",
+          textTransform: "uppercase",
+        }}
+      >
+        We Are Creatives
+      </h1>
+      <img src="./images/icon-arrow-down.svg" alt="down arrow" />
+    </div>
+  );
+};
+
+export default Hero;
