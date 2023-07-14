36aee3458ec43003e1424fab91da3ab9ec2c6bc1
Christal Bell <christalvictoriabell@gmail.com>
2023-07-13 18:32:43 -0700

    header and background image

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import "./App.css";
 import Header from "./components/Header";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,33 @@
 import { Container } from "@mui/material";
-import "./Headers.scss";
 
 const Header = () => {
   return (
-    <Container>
-      <ul>
-        <li>sunnyside</li>
-        <li>About</li>
-        <li>Services</li>
-        <li>Projects</li>
-        <li>Contact</li>
-      </ul>
-    </Container>
+    <div
+      style={{
+        backgroundImage: "url(./images/desktop/image-header.jpg)",
+        backgroundPosition: "center",
+        backgroundSize: "contain",
+        height: "90vh",
+      }}
+    >
+      <Container>
+        <ul
+          style={{
+            listStyleType: "none",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <li>sunnyside</li>
+          <div style={{ display: "flex" }}>
+            <li>About</li>
+            <li style={{ marginLeft: "1rem" }}>Services</li>
+            <li style={{ marginLeft: "1rem" }}>Projects</li>
+            <li style={{ marginLeft: "1rem" }}>Contact</li>
+          </div>
+        </ul>
+      </Container>
+    </div>
   );
 };
 
