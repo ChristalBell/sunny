4bd3a2ff15407bd59587762830a3fc8d34ee9e13
Christal Bell <christalvictoriabell@gmail.com>
2023-07-22 20:15:57 -0700

    testimonial section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
+import Info from "./components/Info";
+import Testimonials from "./components/Testimonials";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Info />
+      <Testimonials />
     </div>
   );
 };
